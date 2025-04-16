fd7b4e9429974aa6b882f2f83cb81b707334c35b
Pavel Shchepko <pavel.shchepko@batmaid.com>
2025-04-16 21:41:14 +0200

    save logs in file

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -1,5 +1,16 @@
 import pino from "pino";
+import systemPath from "path";
 
 export const Logger = pino({
   name: "design-patterns-homework",
+  transport: {
+    target: "pino/file",
+    options: {
+      destination:
+        process.env.NODE_ENV === "test"
+          ? undefined
+          : systemPath.resolve(__dirname, "log.txt"),
+      mkdir: process.env.NODE_ENV !== "test",
+    },
+  },
 });
